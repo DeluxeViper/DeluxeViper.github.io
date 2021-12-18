5b9b79d6dc0e6df381290acd9067fdfedc41a277
CeruleanSource <abdullah.mohamed@ryerson.ca>
2021-12-17 23:06:17 -0500

    feat(techn): technologies config

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,54 +1,103 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+/** @format */
 
-const Technologies = () =>  (
+import React from "react";
+import {
+  DiFirebase,
+  DiReact,
+  DiMysql,
+  DiSqllite,
+  DiJava,
+  DiAndroid,
+} from "react-icons/di";
+import { SiSpring, SiMongodb } from "react-icons/si";
+import { FiDatabase } from "react-icons/fi";
+import { GoServer } from "react-icons/go";
+import { FaVaadin } from "react-icons/fa";
+import { AiOutlineMobile } from "react-icons/ai";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
+
+const Technologies = () => (
   <Section id="tech">
-    <SectionDivider/>
-    <br/>
-    <SectionTitle>
-      Technologies
-    </SectionTitle>
-    <SectionText>
-      Describe the technologies you've worked with!
-    </SectionText>
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText></SectionText>
     <List>
       <ListItem>
-        <DiReact size="3rem"/>
+        <DiReact size="4rem" />
         <ListContainer>
-        <ListTitle>
-          Front-End
-        </ListTitle>
-        <ListParagraph>
-          Experience with <br/> 
-          React.js
-        </ListParagraph>
-      </ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            <li>
+              <DiReact /> React
+            </li>
+            <li>
+              <FaVaadin /> Vaadin
+            </li>
+          </ListParagraph>
+        </ListContainer>
       </ListItem>
       <ListItem>
-        <DiFirebase size="3rem"/>
         <ListContainer>
-        <ListTitle>
-          Back-End
-        </ListTitle>
-        <ListParagraph>
-          Experience with <br/> 
-          Node.js and Databases
-        </ListParagraph>
-      </ListContainer>
+          <GoServer size="3rem" />
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            {/* Experience with <br />
+            The Spring Framework, J2EE */}
+            <li>
+              <SiSpring /> The Spring Framework
+            </li>
+            <li>
+              <DiJava /> J2EE
+            </li>
+          </ListParagraph>
+        </ListContainer>
       </ListItem>
       <ListItem>
-        <DiZend size="3rem"/>
         <ListContainer>
-        <ListTitle>
-          UI/UX
-        </ListTitle>
-        <ListParagraph>
-          Experience with <br/> 
-          Figma
-        </ListParagraph>
-      </ListContainer>
+          <FiDatabase size="3rem" />
+          <ListTitle>Databases</ListTitle>
+          <ListParagraph>
+            {/* SQL (MySQL, PostgreSQL, DB2, SQLite), NoSQL (Firebase RT DB,
+            MongoDB) */}
+            <li>
+              <DiMysql /> MySQL
+            </li>
+            <li>
+              <DiSqllite /> SQLite
+            </li>
+            <li>
+              <DiFirebase /> Firebase Real Time Database
+            </li>
+            <li>
+              <SiMongodb /> MongoDB
+            </li>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <ListContainer>
+          <AiOutlineMobile size="3rem" />
+          <ListTitle>Others</ListTitle>
+          <ListParagraph>
+            <li>
+              <DiAndroid /> Android
+            </li>
+            <li></li>
+          </ListParagraph>
+        </ListContainer>
       </ListItem>
     </List>
   </Section>
